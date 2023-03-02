7f6857e6d4d184483ceddeb9544b92f5c2807cd3
sagar <sagarrsajjan@gmail.com>
2023-03-02 20:52:52 +0530

    my second commit

--- src/test/java/parallel_execution/Programme_Parallel1.java
+++ src/test/java/parallel_execution/Programme_Parallel1.java
@@ -5,8 +5,11 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -17,18 +20,35 @@ public class Programme_Parallel1
 	WebDriver driver;
 	SoftAssert sa = new SoftAssert();
 	@BeforeClass
-	void setup()
+	@Parameters({"browser","url"})
+	void setup(String br,String appurl)
 	{
+		if(br.equals("chrome"))
+		{
 		WebDriverManager.chromedriver().setup();
 		driver= new ChromeDriver();
+		}
+		else if(br.equals("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		else
+		{
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(appurl);
 		
 	}
+	
 	@Test(priority = 1)
 	void login()
 	{
-		driver.get("http://localhost/login.do");
+		
+		
 		driver.findElement(By.id("username")).sendKeys("admin");
 		
 		driver.findElement(By.name("pwd")).sendKeys("manager");
@@ -38,7 +58,7 @@ public class Programme_Parallel1
 	@Test(priority = 2)
 	void title()
 	{
-		String exp_title = "actiTIME - Login";
+		String exp_title = "actiTIME - Enter Time-Track";
 		String act_title = driver.getTitle();
 		System.out.println(act_title);
 		
